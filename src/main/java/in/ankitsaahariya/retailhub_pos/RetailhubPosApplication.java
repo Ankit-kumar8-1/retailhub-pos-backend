b035410f6235d20aa9451376f55954d0c52ef308
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-01-09 17:57:21 +0530

    chore: initial project setup with database and core dependencies - Set up Spring Boot project structure - Added core dependencies: Spring Web, Spring Data JPA, Lombok, MySQL Driver - Configured database connection properties - Verified application startup with successful DB connectivity

---
+++ src/main/java/in/ankitsaahariya/retailhub_pos/RetailhubPosApplication.java
@@ -0,0 +1,14 @@
+package in.ankitsaahariya.retailhub_pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailhubPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailhubPosApplication.class, args);
+        System.out.println("Application is running ....");
+	}
+
+}
---
+++ src/test/java/in/ankitsaahariya/retailhub_pos/RetailhubPosApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ankitsaahariya.retailhub_pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailhubPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
